75deb586a049f239b9e0508abb9b0860a1415d28
Lucas Bickel <116588+hairmare@users.noreply.github.com>
2022-03-17 18:38:32 +0100

    chore: use shorthand in columns

--- app/handler.go
+++ app/handler.go
@@ -60,10 +60,10 @@ func (h *Handler) HandleEvent(event cloudevents.Event) error {
 	h.database.WithContext(ctx).Clauses(
 		clause.OnConflict{
 			Columns: []clause.Column{
-				clause.Column{Name: "cluster_name"},
-				clause.Column{Name: "instance_id"},
-				clause.Column{Name: "application_name"},
-				clause.Column{Name: "application_instance"},
+				{Name: "cluster_name"},
+				{Name: "instance_id"},
+				{Name: "application_name"},
+				{Name: "application_instance"},
 			},
 			UpdateAll: true,
 		},
